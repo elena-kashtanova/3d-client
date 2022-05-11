85cd9cc845e3a4788c039a27b314657df89e5733
Elena Kashtanova <e.kashtanova@andersenlab.com>
2022-05-11 15:29:00 +0300

    Configure routes to pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
+import { ModelLibraryPage } from './pages/ModelLibraryPage';
+import { EditorPage } from './pages/EditorPage';
+import { NotFoundPage } from './pages/NotFoundPage';
+import { Layout } from './components/Layout';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />}></Route>
+        <Route path="models" element={<ModelLibraryPage />}></Route>
+        <Route path="models/:id" element={<EditorPage />}></Route>
+        <Route path="*" element={<NotFoundPage />}></Route>
+      </Route>
+    </Routes>
   );
 }
 

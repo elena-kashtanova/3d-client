9df6aded7c38ec06f12f95d8afa4cb468ada1478
Elena Kashtanova <e.kashtanova@andersenlab.com>
2022-05-11 16:54:43 +0300

    Type Model context

--- src/components/ModelContext.tsx
+++ src/components/ModelContext.tsx
@@ -1,5 +1,17 @@
 import { createContext } from 'react';
 
-const ModelContext = createContext(null);
+interface IModelContext {
+  id: string;
+  name: string;
+  type: string;
+  color: string;
+  position: number[];
+  uv: number[];
+  normal: number[];
+  index: number[];
+  parameters: { [key: string]: any } | null;
+}
+
+const ModelContext = createContext<IModelContext | null>(null);
 
 export { ModelContext };

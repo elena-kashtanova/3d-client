a8d3d1069146e264ff7b7773aefffc22273ab3a5
Elena Kashtanova <e.kashtanova@andersenlab.com>
2022-05-11 17:13:23 +0300

    Display selected object in viewport

--- src/components/Viewport.tsx
+++ src/components/Viewport.tsx
@@ -1,21 +1,63 @@
-import { useContext } from 'react';
+import * as THREE from 'three';
+import { useContext, useRef } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { ModelContext } from './ModelContext';
+import { OrbitControls, TransformControls } from '@react-three/drei';
 
 function Viewport() {
   const data = useContext(ModelContext);
+  // Get access to the Mesh object
+  const mesh = useRef<THREE.Mesh>(null!);
 
   return (
     <div className="viewport">
-      <Canvas>
+      <Canvas camera={{ fov: 35, position: [10, 10, 10] }}>
+        <OrbitControls
+          makeDefault
+          enablePan={true}
+          enableZoom={true}
+          enableRotate={true}
+        />
         <ambientLight intensity={0.5} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-        <pointLight position={[-10, -10, -10]} />
-        <mesh>
-          <bufferGeometry>
-            <bufferAttribute />
-          </bufferGeometry>
-        </mesh>
+        <directionalLight position={[15, 20, 25]} intensity={1} />
+        <TransformControls object={mesh} mode="scale" />
+        {data && (
+          <mesh ref={mesh} position={[0, 0, 0]} scale={1}>
+            <meshPhongMaterial
+              attach="material"
+              color={parseInt(data.color, 16)}
+              side={THREE.DoubleSide}
+            />
+            <bufferGeometry>
+              {data.index && (
+                <bufferAttribute
+                  array={new Uint32Array(data.index)}
+                  attach="index"
+                  count={data.index.length}
+                  itemSize={1}
+                />
+              )}
+              <bufferAttribute
+                attach="attributes-position"
+                count={data.position.length / 3}
+                itemSize={3}
+                array={new Float32Array(data.position)}
+              />
+              <bufferAttribute
+                attach="attributes-normal"
+                count={data.normal.length / 3}
+                itemSize={3}
+                array={new Float32Array(data.normal)}
+              />
+              <bufferAttribute
+                attach="attributes-uv"
+                count={data.uv.length / 2}
+                itemSize={2}
+                array={new Float32Array(data.uv)}
+              />
+            </bufferGeometry>
+          </mesh>
+        )}
       </Canvas>
     </div>
   );

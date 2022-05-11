7269777a4665676a49e9a3c17b9efa58f8828de6
Elena Kashtanova <e.kashtanova@andersenlab.com>
2022-05-11 16:11:02 +0300

    Fetch all models from api

--- src/pages/ModelLibraryPage.tsx
+++ src/pages/ModelLibraryPage.tsx
@@ -1,9 +1,39 @@
+import { useEffect, useState } from 'react';
 import { ModelList } from '../components/ModelList';
 
 function ModelLibraryPage() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const url = `${process.env.REACT_APP_API_URL}/models`;
+
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Error ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setData(data);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setData([]);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <main className="content">
-      <ModelList />
+      {loading && <p>Loading...</p>}
+      {error && <h1>{`Something went wrongs - ${error}`}</h1>}
+      {data && <ModelList data={data} />}
     </main>
   );
 }
